23d1a20a588881201940f27d6ec7c76349a23532
Omega-Nexus-arch <149561344+Omega-Nexus-arch@users.noreply.github.com>
2024-12-05 06:00:05 -0500

    Add files via upload

---
+++ eyrc_gitpull.py
@@ -0,0 +1,8 @@
+Methoid to pull new work for eyrc:
+
+git status (check)
+git add . (add all the untracked files)
+git status (to verify)
+git stash (to save your present work in a stack locally)
+git checkouot tags (as per ask given)
+git stash pop (to again bring your changes back locally)
